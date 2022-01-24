9ba8db5322f8fd1d80ee8d3810d139154755ef9d
Jeffrey Siu <jeffythecashless@gmail.com>
2022-01-24 18:39:22 -0500

    exp: add image loader

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    loader: 'imgix',
+    path: '/'
+  }
 }
 
 module.exports = nextConfig
