b1fef9826edab76133922852bb1ba1a5db4360d8
Jeffrey Siu <jeffythecashless@gmail.com>
2022-01-24 17:54:28 -0500

    chore: move source code up one level

--- components/navbutton.tsx
+++ components/navbutton.tsx
@@ -1,17 +1,15 @@
 import Link from "next/link";
 import Button from "@mui/material/Button";
 
-type NavButtonProps = {
+export type NavButtonProps = {
   route: string;
   navigationText: string;
 };
 
-function NavButton({ route, navigationText }: NavButtonProps) {
+export function NavButton({ route, navigationText }: NavButtonProps) {
   return (
     <Link href={route} passHref>
       <Button variant="outlined" color="inherit">{navigationText}</Button>
     </Link>
   );
 }
-
-export { NavButton, NavButtonProps };
